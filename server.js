769d18d607fb81f84001ff0f43547eda99863112
Jason L Perry <jasper@ambethia.com>
2017-08-07 11:28:47 -0400

    Update
    A lazy commit.

---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require('express')()
+const pgp = require('pg-promise')()
+
+const db = pgp('postgres://localhost:5432/adventure-time');
+
+app.get('/characters', (req, res) => {
+  db.any('SELECT id, name FROM characters')
+    .then((data) => {
+      res.json(data)
+    })
+})
+
+app.get('/characters/:id', (req, res) => {
+  const id = req.params.id
+  db.oneOrNone('SELECT * FROM characters WHERE id = $1', [id])
+    .then((data) => {
+      res.json(data)
+    })
+})
+
+app.listen(3000, () => {
+  console.log('Algebraeic!')
+})
